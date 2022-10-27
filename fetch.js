54ed645a7a746f36d35f3b6a9860c17f5255ccfe
abhishek305 <abhishekshaji1994@gmail.com>
2022-10-27 20:59:14 +0530

    fix:test3

--- fetch.js
+++ fetch.js
@@ -211,25 +211,13 @@ var fetchCsData = /*#__PURE__*/function () {
                   console.error(data);
                   reject(data);
                 } else {
-                  // if (data) {
                   if (data.items) {
-                    var filteredArray = data === null || data === void 0 ? void 0 : data.items.filter(function (item) {
+                    var filteredData = data === null || data === void 0 ? void 0 : data.items.filter(function (item) {
                       return item.data.hasOwnProperty('publish_details');
                     });
-                    data.items = filteredArray;
-                  } // data?.items?.map((item, index) => {
-                  //   if (item.data.hasOwnProperty('publish_details')) {
-                  //     // data?.items?.splice(index, 1);
-                  //     // logger?.info('Testing warning');
-                  //     console.log('testing');
-                  //   } else {
-                  //     data?.items?.splice(index, 1);
-                  //     logger?.info('Inside condition');
-                  //   }
-                  // });
+                    data.items = filteredData;
+                  }
 
-
-                  console.log('logg', data);
                   resolve(data);
                 }
               })["catch"](function (err) {
