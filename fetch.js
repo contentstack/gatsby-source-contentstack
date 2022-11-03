3e55ee3f9e2329e84ffed274ef788ff684c3d67d
abhishek305 <abhishekshaji1994@gmail.com>
2022-11-03 18:10:42 +0530

    feat:http-retries & increment delays

--- fetch.js
+++ fetch.js
@@ -58,7 +58,7 @@ var OPTIONS_ENTRIES_CLASS_MAPPING = {
   excludeContentTypeslocales: FetchSpecifiedLocalesAndContentTypesEntries
 };
 var activity;
-var logger;
+var globalConfig;
 
 exports.fetchData = /*#__PURE__*/function () {
   var _ref = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(configOptions, reporter, cache, contentTypeOption) {
@@ -68,17 +68,16 @@ exports.fetchData = /*#__PURE__*/function () {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            logger = reporter;
             activity = reporter.activityTimer("Fetching Contentstack data");
             activity.start();
             activity.setStatus('Starting to fetch data from Contentstack');
-            _context.prev = 4;
+            _context.prev = 3;
             syncData = {};
             entryService = new OPTIONS_ENTRIES_CLASS_MAPPING[contentTypeOption]();
-            _context.next = 9;
+            _context.next = 8;
             return entryService.fetchSyncData(configOptions, cache, fetchSyncData);
 
-          case 9:
+          case 8:
             _syncData = _context.sent;
             syncData.data = _syncData.data;
             contentstackData = {
@@ -89,9 +88,9 @@ exports.fetchData = /*#__PURE__*/function () {
               contentstackData: contentstackData
             });
 
-          case 16:
-            _context.prev = 16;
-            _context.t0 = _context["catch"](4);
+          case 15:
+            _context.prev = 15;
+            _context.t0 = _context["catch"](3);
             reporter.panic({
               id: CODES.SyncError,
               context: {
@@ -100,12 +99,12 @@ exports.fetchData = /*#__PURE__*/function () {
               error: _context.t0
             });
 
-          case 19:
+          case 18:
           case "end":
             return _context.stop();
         }
       }
-    }, _callee, null, [[4, 16]]);
+    }, _callee, null, [[3, 15]]);
   }));
 
   return function (_x, _x2, _x3, _x4) {
@@ -120,21 +119,22 @@ exports.fetchContentTypes = /*#__PURE__*/function () {
       while (1) {
         switch (_context2.prev = _context2.next) {
           case 0:
-            _context2.prev = 0;
+            globalConfig = config;
+            _context2.prev = 1;
             config.cdn = config.cdn ? config.cdn : 'https://cdn.contentstack.io/v3';
             url = 'content_types';
             responseKey = 'content_types';
             contentType = new OPTION_CLASS_MAPPING[contentTypeOption]();
-            _context2.next = 7;
+            _context2.next = 8;
             return contentType.getPagedData(url, config, responseKey, getPagedData);
 
-          case 7:
+          case 8:
             allContentTypes = _context2.sent;
             return _context2.abrupt("return", allContentTypes);
 
-          case 11:
-            _context2.prev = 11;
-            _context2.t0 = _context2["catch"](0);
+          case 12:
+            _context2.prev = 12;
+            _context2.t0 = _context2["catch"](1);
             reporter.panic({
               id: CODES.SyncError,
               context: {
@@ -143,12 +143,12 @@ exports.fetchContentTypes = /*#__PURE__*/function () {
               error: _context2.t0
             });
 
-          case 14:
+          case 15:
           case "end":
             return _context2.stop();
         }
       }
-    }, _callee2, null, [[0, 11]]);
+    }, _callee2, null, [[1, 12]]);
   }));
 
   return function (_x5, _x6) {
@@ -184,12 +184,108 @@ var fetchSyncData = /*#__PURE__*/function () {
   };
 }();
 
-var fetchCsData = /*#__PURE__*/function () {
-  var _ref4 = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee4(url, config, query) {
-    var queryParams, apiUrl, option;
-    return _regenerator["default"].wrap(function _callee4$(_context4) {
+function waitFor(milliseconds) {
+  return new Promise(function (resolve) {
+    return setTimeout(resolve, milliseconds);
+  });
+}
+
+var getData = /*#__PURE__*/function () {
+  var _ref4 = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee5(url, options) {
+    var retries;
+    return _regenerator["default"].wrap(function _callee5$(_context5) {
       while (1) {
-        switch (_context4.prev = _context4.next) {
+        switch (_context5.prev = _context5.next) {
+          case 0:
+            retries = 0;
+            return _context5.abrupt("return", new Promise(function (resolve, reject) {
+              var handleResponse = function handleResponse() {
+                fetch(url, options).then(function (response) {
+                  return response.json();
+                }).then(function (data) {
+                  if (data.error_code) {
+                    console.error(data);
+
+                    if (data.error_code >= 500) {
+                      throw new Error("Server error: ".concat(data.error_code));
+                    }
+
+                    reject(data);
+                  } else {
+                    if (data.items) {
+                      var filteredData = data === null || data === void 0 ? void 0 : data.items.filter(function (item) {
+                        return item.data.hasOwnProperty('publish_details');
+                      });
+                      data.items = filteredData;
+                    }
+
+                    resolve(data);
+                  }
+                })["catch"]( /*#__PURE__*/function () {
+                  var _ref5 = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee4(err) {
+                    var retryAttempt, timeToWait;
+                    return _regenerator["default"].wrap(function _callee4$(_context4) {
+                      while (1) {
+                        switch (_context4.prev = _context4.next) {
+                          case 0:
+                            retryAttempt = globalConfig.httpRetries ? globalConfig.httpRetries : 3;
+
+                            if (!(retries < retryAttempt)) {
+                              _context4.next = 9;
+                              break;
+                            }
+
+                            retries++;
+                            timeToWait = Math.pow(2, retries) * 100;
+                            _context4.next = 6;
+                            return waitFor(timeToWait);
+
+                          case 6:
+                            handleResponse();
+                            _context4.next = 11;
+                            break;
+
+                          case 9:
+                            console.error(err);
+                            reject(new Error("Fetch failed after ".concat(retryAttempt, " retry attempts.")));
+
+                          case 11:
+                          case "end":
+                            return _context4.stop();
+                        }
+                      }
+                    }, _callee4);
+                  }));
+
+                  return function (_x11) {
+                    return _ref5.apply(this, arguments);
+                  };
+                }());
+              };
+
+              retries = 1;
+              handleResponse();
+            }));
+
+          case 2:
+          case "end":
+            return _context5.stop();
+        }
+      }
+    }, _callee5);
+  }));
+
+  return function getData(_x9, _x10) {
+    return _ref4.apply(this, arguments);
+  };
+}();
+
+var fetchCsData = /*#__PURE__*/function () {
+  var _ref6 = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee6(url, config, query) {
+    var queryParams, apiUrl, option, data;
+    return _regenerator["default"].wrap(function _callee6$(_context6) {
+      while (1) {
+        switch (_context6.prev = _context6.next) {
           case 0:
             query = query || {};
             query.include_count = true;
@@ -203,66 +299,50 @@ var fetchCsData = /*#__PURE__*/function () {
                 access_token: config.delivery_token
               }
             };
-            return _context4.abrupt("return", new Promise(function (resolve, reject) {
-              fetch(apiUrl, option).then(function (response) {
-                return response.json();
-              }).then(function (data) {
-                if (data.error_code) {
-                  console.error(data);
-                  reject(data);
-                } else {
-                  if (data.items) {
-                    var filteredData = data === null || data === void 0 ? void 0 : data.items.filter(function (item) {
-                      return item.data.hasOwnProperty('publish_details');
-                    });
-                    data.items = filteredData;
-                  }
+            _context6.next = 8;
+            return getData(apiUrl, option);
 
-                  resolve(data);
-                }
-              })["catch"](function (err) {
-                console.error(err);
-                reject(err);
-              });
-            }));
+          case 8:
+            data = _context6.sent;
+            return _context6.abrupt("return", data);
 
-          case 7:
+          case 10:
           case "end":
-            return _context4.stop();
+            return _context6.stop();
         }
       }
-    }, _callee4);
+    }, _callee6);
   }));
 
-  return function fetchCsData(_x9, _x10, _x11) {
-    return _ref4.apply(this, arguments);
+  return function fetchCsData(_x12, _x13, _x14) {
+    return _ref6.apply(this, arguments);
   };
 }();
 
 var getPagedData = /*#__PURE__*/function () {
-  var _ref5 = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee5(url, config, responseKey) {
+  var _ref7 = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee7(url, config, responseKey) {
     var query,
         skip,
         limit,
         aggregatedResponse,
         response,
-        _args5 = arguments;
-    return _regenerator["default"].wrap(function _callee5$(_context5) {
+        _args7 = arguments;
+    return _regenerator["default"].wrap(function _callee7$(_context7) {
       while (1) {
-        switch (_context5.prev = _context5.next) {
+        switch (_context7.prev = _context7.next) {
           case 0:
-            query = _args5.length > 3 && _args5[3] !== undefined ? _args5[3] : {};
-            skip = _args5.length > 4 && _args5[4] !== undefined ? _args5[4] : 0;
-            limit = _args5.length > 5 && _args5[5] !== undefined ? _args5[5] : 100;
-            aggregatedResponse = _args5.length > 6 && _args5[6] !== undefined ? _args5[6] : null;
+            query = _args7.length > 3 && _args7[3] !== undefined ? _args7[3] : {};
+            skip = _args7.length > 4 && _args7[4] !== undefined ? _args7[4] : 0;
+            limit = _args7.length > 5 && _args7[5] !== undefined ? _args7[5] : 100;
+            aggregatedResponse = _args7.length > 6 && _args7[6] !== undefined ? _args7[6] : null;
             query.skip = skip;
             query.limit = limit;
             query.include_global_field_schema = true;
-            _context5.next = 9;
+            _context7.next = 9;
             return fetchCsData(url, config, query);
 
           case 9:
-            response = _context5.sent;
+            response = _context7.sent;
 
             if (!aggregatedResponse) {
               aggregatedResponse = response[responseKey];
@@ -271,43 +351,43 @@ var getPagedData = /*#__PURE__*/function () {
             }
 
             if (!(skip + limit <= response.count)) {
-              _context5.next = 13;
+              _context7.next = 13;
               break;
             }
 
-            return _context5.abrupt("return", getPagedData(url, config, responseKey, query = {}, skip + limit, limit, aggregatedResponse));
+            return _context7.abrupt("return", getPagedData(url, config, responseKey, query = {}, skip + limit, limit, aggregatedResponse));
 
           case 13:
-            return _context5.abrupt("return", aggregatedResponse);
+            return _context7.abrupt("return", aggregatedResponse);
 
           case 14:
           case "end":
-            return _context5.stop();
+            return _context7.stop();
         }
       }
-    }, _callee5);
+    }, _callee7);
   }));
 
-  return function getPagedData(_x12, _x13, _x14) {
-    return _ref5.apply(this, arguments);
+  return function getPagedData(_x15, _x16, _x17) {
+    return _ref7.apply(this, arguments);
   };
 }();
 
 var getSyncData = /*#__PURE__*/function () {
-  var _ref6 = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee6(url, config, query, responseKey) {
+  var _ref8 = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee8(url, config, query, responseKey) {
     var aggregatedResponse,
         response,
-        _args6 = arguments;
-    return _regenerator["default"].wrap(function _callee6$(_context6) {
+        _args8 = arguments;
+    return _regenerator["default"].wrap(function _callee8$(_context8) {
       while (1) {
-        switch (_context6.prev = _context6.next) {
+        switch (_context8.prev = _context8.next) {
           case 0:
-            aggregatedResponse = _args6.length > 4 && _args6[4] !== undefined ? _args6[4] : null;
-            _context6.next = 3;
+            aggregatedResponse = _args8.length > 4 && _args8[4] !== undefined ? _args8[4] : null;
+            _context8.next = 3;
             return fetchCsData(url, config, query);
 
           case 3:
-            response = _context6.sent;
+            response = _context8.sent;
 
             if (!aggregatedResponse) {
               aggregatedResponse = {};
@@ -321,26 +401,26 @@ var getSyncData = /*#__PURE__*/function () {
             }
 
             if (!response.pagination_token) {
-              _context6.next = 7;
+              _context8.next = 7;
               break;
             }
 
-            return _context6.abrupt("return", getSyncData(url, config, query = {
+            return _context8.abrupt("return", getSyncData(url, config, query = {
               pagination_token: response.pagination_token
             }, responseKey, aggregatedResponse));
 
           case 7:
-            return _context6.abrupt("return", aggregatedResponse);
+            return _context8.abrupt("return", aggregatedResponse);
 
           case 8:
           case "end":
-            return _context6.stop();
+            return _context8.stop();
         }
       }
-    }, _callee6);
+    }, _callee8);
   }));
 
-  return function getSyncData(_x15, _x16, _x17, _x18) {
-    return _ref6.apply(this, arguments);
+  return function getSyncData(_x18, _x19, _x20, _x21) {
+    return _ref8.apply(this, arguments);
   };
 }();
--- plugin-options-schema.js
+++ plugin-options-schema.js
@@ -18,6 +18,7 @@ exports.pluginOptionsSchema = function (_ref) {
     contentTypes: Joi.array().items(Joi.string().required()).description("Specify list of content-types to be fetched from contentstack"),
     excludeContentTypes: Joi.array().items(Joi.string().required()).description("Specify list of content-types to be excluded while fetching data from contentstack"),
     locales: Joi.array().items(Joi.string().required()).description("Specify list of locales to be fetched from contentstack"),
-    jsonRteToHtml: Joi["boolean"]()["default"](false).description("Specify true if you want to generate html from json RTE field")
+    jsonRteToHtml: Joi["boolean"]()["default"](false).description("Specify true if you want to generate html from json RTE field"),
+    httpRetries: Joi.number().integer()["default"](3).description("Specify the number of times to perform http request on a network failure")
   }).external(validateContentstackAccess);
 };
