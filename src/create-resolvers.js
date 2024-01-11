d36bff797911fc8fc156753dd926c7045a6212c5
Faraaz Biyabani <faraaz.biyabani@contentstack.com>
2024-01-11 12:01:18 +0530

    feat: cslp resolver WIP

--- src/create-resolvers.js
+++ src/create-resolvers.js
@@ -16,38 +16,200 @@ exports.createResolvers = async ({ createResolvers, cache, createNodeId }, confi
     cache.get(`${typePrefix}_${configOptions.api_key}_json_rte_fields`),
   ]);
 
-  fileFields && fileFields.forEach(fileField => {
-    resolvers[fileField.parent] = {
-      ...resolvers[fileField.parent],
-      ... {
-        [fileField.field.uid]: {
-          resolve(source, args, context) {
-            if (fileField.field.multiple && source[`${fileField.field.uid}___NODE`]) {
-              const nodesData = [];
-
-              source[`${fileField.field.uid}___NODE`].forEach(id => {
-                const existingNode = context.nodeModel.getNodeById({ id });
-
-                if (existingNode) {
-                  nodesData.push(existingNode);
-                }
-              });
-
-              return nodesData;
-            } else {
-              const id = source[`${fileField.field.uid}___NODE`];
-              return context.nodeModel.getNodeById({ id });
-            }
-          },
-        },
-      }
-    };
+  // for each of these content types create a root field cslp_meta
+  // which resolves with LP metadata
+  // typePrefix_contenttypeUId
+  const contentTypes = await cache.get(typePrefix);
+  const contentTypeMap = {};
+  contentTypes.forEach((item) => {
+    contentTypeMap[item.uid] = item;
   })
+
+  // function collectReferences(currentPath = "", selectionSet, schema) {
+  //   let referencePaths = [];
+  //   if (schema.data_type === "reference") {
+  //     referencePaths.push(currentPath.concat(".", schema.uid))
+  //   }
+  //   if (selectionSet?.selections.length) {
+  //     return collectReferences(schema.uid, selectionSet)
+  //   }
+  // }
+
+  function getCslpMetaPaths(selectionSet, path = "", schema, fragments) {
+    let referencePaths = [];
+    if (schema.data_type === "reference" && path) {
+      referencePaths.push(path)
+    }
+    for (const selection of selectionSet.selections) {
+      // exit when selection.kind is not Field, SelectionSet or InlineFragment
+      // selection.name is not present for selection.kind is "InlineFragment"
+      if (selection?.name?.value === "cslp__meta") {
+        continue;
+      }
+      if (selection.selectionSet || selection.kind === "FragmentSpread") {
+        let nestedFields = schema?.blocks ?? schema.schema;
+        // selection.kind === "InlineFragment" && selection.typeCondition.name.value
+        if (selection.kind === "FragmentSpread") {
+          const fragmentName = selection.name.value;
+          if (fragmentName in fragments) {
+            const fragmentDefinition = fragments[fragmentName];
+            const refPaths = getCslpMetaPaths(fragmentDefinition.selectionSet, path, schema, fragments);
+            referencePaths.push(...refPaths);
+          }
+        }
+        const inlineFragmentNodeType = selection.typeCondition?.name?.value;
+        if (selection.kind === "InlineFragment" && inlineFragmentNodeType) {
+          const contentTypeUid = inlineFragmentNodeType.replace(`${typePrefix}_`, "");
+          if (contentTypeUid) {
+            const contentTypeSchema = contentTypeMap[contentTypeUid];
+            const refPaths = getCslpMetaPaths(selection.selectionSet, path, contentTypeSchema, fragments);
+            referencePaths.push(...refPaths);
+          }
+        } else {
+          // block
+          if (schema.reference_to) {
+            nestedFields = contentTypeMap[schema.reference_to[0]].schema
+          }
+          const nestedFieldSchema = nestedFields.find((item) => item.uid === selection.name.value);
+          if (nestedFieldSchema) {
+            let nextPath = []
+            if (path) {
+              nextPath = path.split(".")
+            }
+            nextPath.push(selection.name.value)
+            const nestedReferencePaths = getCslpMetaPaths(selection.selectionSet, nextPath.join("."), nestedFieldSchema, fragments);
+            referencePaths.push(...nestedReferencePaths);
+          }
+        }
+      }
+    }
+    return referencePaths;
+  }
+
+  function getQueryContentTypeSelection(selectionSet, value, location, depth = 0) {
+    // don't need to search for more than one level deep
+    // as cslp__meta can only be one level deeper
+    // e.g.
+    // query {
+    //   page {
+    //     cslp__meta
+    //   }
+    //   blog {
+    //     cslp__meta
+    //   }
+    // }
+    if (depth > 1 || !selectionSet || !selectionSet.selections) {
+      return;
+    }
+    for (const selection of selectionSet.selections) {
+      if (
+        selection.name?.value === value &&
+        selection.loc?.start === location.start &&
+        selection.loc?.end === location.end
+      ) {
+        return selectionSet
+      }
+      // search one level deeper for the correct node in this selection
+      const nestedSelectionSet = getQueryContentTypeSelection(selection.selectionSet, value, location, depth + 1)
+      // return when not undefined, meaning the correct selection has been found
+      if (nestedSelectionSet) {
+        return nestedSelectionSet;
+      }
+
+    }
+  }
+
+  contentTypes.forEach((contentType) => {
+    // const name = `${typePrefix}_${contentTypeUid}`;
+    resolvers[`${typePrefix}_${contentType.uid}`] = {
+      "cslp__meta": {
+        type: "JSON",
+        resolve(source, args, context, info) {
+          // console.log(source)
+          // const source = JSON.parse(JSON.stringify(sourceCopy));
+          const entryUid = source.uid;
+          const contentTypeNodeType = source.internal.type
+          const queryContentTypeUid = contentTypeNodeType.replace(`${typePrefix}_`, "")
+
+          const fieldNode = info.fieldNodes.find((node) => node.name?.value === "cslp__meta");
+          const fieldNodeValue = fieldNode.name?.value;
+          const fieldNodeLocation = { start: fieldNode.name?.loc?.start, end: fieldNode.name?.loc?.end }
+          const queryContentTypeSelection = getQueryContentTypeSelection(info.operation.selectionSet, fieldNodeValue, fieldNodeLocation)
+
+          const contentType = contentTypeMap[queryContentTypeUid];
+          const firstOperation = info.operation.selectionSet.selections[0];
+          const paths = {}
+          // for (const selection of firstOperation.selectionSet.selections) {
+          //   if (selection.name.value === "cslp__meta") {
+          //     continue;
+          //   }
+          //   const queryField = selection.name.value;
+          //   // const fieldSchema = contentType.schema.find((field) => field.uid === queryField)
+          //   // if (
+          //   //   !fieldSchema ||
+          //   //   fieldSchema.data_type !== "blocks" ||
+          //   //   fieldSchema.data_type !== "group" ||
+          //   //   fieldSchema.data_type !== "reference" ||
+          //   //   fieldSchema.data_type !== "global_field"
+          //   // ) {
+          //   //   continue;
+          //   // }
+          //   paths[entryUid] = someName(selection.selectionSet, queryField, contentType)
+          // }
+          paths[entryUid] = {
+            "referencePaths": getCslpMetaPaths(queryContentTypeSelection, "", contentType, info?.fragments ?? {})
+          }
+          // const operation = info.operation;
+          console.log(info.operation)
+          return paths;
+        }
+      }
+    }
+  })
+
+  resolvers["Query"] = {
+    "cslp__meta": {
+      type: "JSON",
+      resolve(source, args, context, info) {
+        // const operation = info.operation;
+        console.log(info.operation)
+        return JSON.stringify(info.operation)
+      }
+    }
+  },
+
+    fileFields && fileFields.forEach(fileField => {
+      resolvers[fileField.parent] = {
+        ...resolvers[fileField.parent],
+        ... {
+          [fileField.field.uid]: {
+            resolve(source, args, context) {
+              if (fileField.field.multiple && source[`${fileField.field.uid}___NODE`]) {
+                const nodesData = [];
+
+                source[`${fileField.field.uid}___NODE`].forEach(id => {
+                  const existingNode = context.nodeModel.getNodeById({ id });
+
+                  if (existingNode) {
+                    nodesData.push(existingNode);
+                  }
+                });
+
+                return nodesData;
+              } else {
+                const id = source[`${fileField.field.uid}___NODE`];
+                return context.nodeModel.getNodeById({ id });
+              }
+            },
+          },
+        }
+      };
+    })
   references && references.forEach(reference => {
     resolvers[reference.parent] = {
       ...resolvers[reference.parent],
       [reference.uid]: {
-        resolve(source, args, context) {
+        resolve(source, args, context, info) {
           if (source[`${reference.uid}___NODE`]) {
             const nodesData = [];
 
--- src/live-preview/index.js
+++ src/live-preview/index.js
@@ -36,30 +36,30 @@ export class ContentstackGatsby {
     this.stackSdk = contentstack.Stack(stackConfig);
 
     // reference fields in various CTs and the CTs they refer
-    this.referenceFieldsStorage = new Storage(
-      window.sessionStorage,
-      'reference_fields'
-    );
-    this.referenceFields = this.referenceFieldsStorage.get();
+    // this.referenceFieldsStorage = new Storage(
+    //   window.sessionStorage,
+    //   'reference_fields'
+    // );
+    // this.referenceFields = this.referenceFieldsStorage.get();
 
     this.statusStorage = new Storage(window.sessionStorage, "status")
 
     // json rte fields in various CTs
-    this.jsonRteFieldsStorage = new Storage(
-      window.sessionStorage,
-      'json_rte_fields'
-    );
-    this.jsonRteFields = this.jsonRteFieldsStorage.get();
+    // this.jsonRteFieldsStorage = new Storage(
+    //   window.sessionStorage,
+    //   'json_rte_fields'
+    // );
+    // this.jsonRteFields = this.jsonRteFieldsStorage.get();
 
-    // only field paths extracted from the above map for current CT
-    this.referenceFieldPaths = [];
+    // // only field paths extracted from the above map for current CT
+    // this.referenceFieldPaths = [];
 
-    // store content types in LP site's session storage
-    this.contentTypesStorage = new Storage(
-      window.sessionStorage,
-      'content_types'
-    );
-    this.contentTypes = this.contentTypesStorage.get();
+    // // store content types in LP site's session storage
+    // this.contentTypesStorage = new Storage(
+    //   window.sessionStorage,
+    //   'content_types'
+    // );
+    // this.contentTypes = this.contentTypesStorage.get();
   }
 
   setHost(host) {
@@ -96,204 +96,206 @@ export class ContentstackGatsby {
     }
   }
 
-  async fetchContentTypes(uids) {
-    try {
-      const result = await this.stackSdk.getContentTypes({
-        query: { uid: { $in: uids } },
-      });
-      if (result) {
-        const contentTypes = {};
-        result.content_types.forEach(ct => {
-          contentTypes[ct.uid] = ct;
-        });
-        return contentTypes;
-      }
-    } catch (error) {
-      console.error('ContentstackGatsby - Failed to fetch content types');
-      throw error;
-    }
-  }
+  // async fetchContentTypes(uids) {
+  //   try {
+  //     const result = await this.stackSdk.getContentTypes({
+  //       query: { uid: { $in: uids } },
+  //     });
+  //     if (result) {
+  //       const contentTypes = {};
+  //       result.content_types.forEach(ct => {
+  //         contentTypes[ct.uid] = ct;
+  //       });
+  //       return contentTypes;
+  //     }
+  //   } catch (error) {
+  //     console.error('ContentstackGatsby - Failed to fetch content types');
+  //     throw error;
+  //   }
+  // }
 
-  async getContentTypes(uids) {
-    // fetch and filter only content types that are not available in cache
-    const uidsToFetch = uids.filter(uid => !this.contentTypes[uid]);
-    if (!uidsToFetch.length) {
-      return this.contentTypes;
-    }
-    const types = await this.fetchContentTypes(uidsToFetch);
-    uidsToFetch.forEach(uid => {
-      // TODO need to set it in two places, can be better
-      this.contentTypes[uid] = types[uid];
-      this.contentTypesStorage.set(uid, types[uid]);
-    });
-    return this.contentTypes;
-  }
+  // async getContentTypes(uids) {
+  //   // fetch and filter only content types that are not available in cache
+  //   const uidsToFetch = uids.filter(uid => !this.contentTypes[uid]);
+  //   if (!uidsToFetch.length) {
+  //     return this.contentTypes;
+  //   }
+  //   const types = await this.fetchContentTypes(uidsToFetch);
+  //   uidsToFetch.forEach(uid => {
+  //     // TODO need to set it in two places, can be better
+  //     this.contentTypes[uid] = types[uid];
+  //     this.contentTypesStorage.set(uid, types[uid]);
+  //   });
+  //   return this.contentTypes;
+  // }
 
-  async extractReferences(
-    refPathMap = {},
-    status,
-    jsonRtePaths = [],
-    depth = MAX_DEPTH_ALLOWED,
-    seen = []
-  ) {
-    if (depth <= 0) {
-      return refPathMap;
-    }
-    const uids = [...new Set(Object.values(refPathMap).flat())];
-    const contentTypes = await this.getContentTypes(uids);
-    const refPathsCount = Object.keys(refPathMap).length;
-    const explorePaths = Object.entries(refPathMap).filter(
-      ([path]) => !seen.includes(path)
-    );
-    for (const [refPath, refUids] of explorePaths) {
-      // mark this reference path as seen
-      seen.push(refPath);
-      for (const uid of refUids) {
-        let rPath = refPath.split('.');
-        // when path is root, set path to []
-        if (refPath === '') {
-          rPath = [];
-        }
+  // async extractReferences(
+  //   refPathMap = {},
+  //   status,
+  //   jsonRtePaths = [],
+  //   depth = MAX_DEPTH_ALLOWED,
+  //   seen = []
+  // ) {
+  //   if (depth <= 0) {
+  //     return refPathMap;
+  //   }
+  //   const uids = [...new Set(Object.values(refPathMap).flat())];
+  //   const contentTypes = await this.getContentTypes(uids);
+  //   const refPathsCount = Object.keys(refPathMap).length;
+  //   const explorePaths = Object.entries(refPathMap).filter(
+  //     ([path]) => !seen.includes(path)
+  //   );
+  //   for (const [refPath, refUids] of explorePaths) {
+  //     // mark this reference path as seen
+  //     seen.push(refPath);
+  //     for (const uid of refUids) {
+  //       let rPath = refPath.split('.');
+  //       // when path is root, set path to []
+  //       if (refPath === '') {
+  //         rPath = [];
+  //       }
 
-        if (!status.hasLivePreviewEntryFound) {
-          status.hasLivePreviewEntryFound = this.isCurrentEntryEdited(uid)
-        }
+  //       if (!status.hasLivePreviewEntryFound) {
+  //         status.hasLivePreviewEntryFound = this.isCurrentEntryEdited(uid)
+  //       }
 
-        this.extractUids(
-          contentTypes[uid].schema,
-          rPath,
-          refPathMap,
-          jsonRtePaths
-        );
-      }
-    }
-    if (Object.keys(refPathMap).length > refPathsCount) {
-      await this.extractReferences(refPathMap, status, jsonRtePaths, depth - 1, seen);
-    }
-    return { refPathMap, jsonRtePaths };
-  }
+  //       this.extractUids(
+  //         contentTypes[uid].schema,
+  //         rPath,
+  //         refPathMap,
+  //         jsonRtePaths
+  //       );
+  //     }
+  //   }
+  //   if (Object.keys(refPathMap).length > refPathsCount) {
+  //     await this.extractReferences(refPathMap, status, jsonRtePaths, depth - 1, seen);
+  //   }
+  //   return { refPathMap, jsonRtePaths };
+  // }
 
-  extractUids(schema, pathPrefix = [], refPathMap = {}, jsonRtePaths = []) {
-    const referredUids = [];
-    for (const field of schema) {
-      const fieldPath = [...pathPrefix, field.uid];
-      if (
-        field.data_type === 'reference' &&
-        Array.isArray(field.reference_to) &&
-        field.reference_to.length > 0
-      ) {
-        referredUids.push(...field.reference_to);
-        refPathMap[fieldPath.join('.')] = field.reference_to;
-      } else if (
-        field.data_type === 'blocks' &&
-        field.blocks &&
-        field.blocks.length > 0
-      ) {
-        for (const block of field.blocks) {
-          const { referredUids: blockRefUids } = this.extractUids(
-            block.schema,
-            [...fieldPath, block.uid],
-            refPathMap,
-            jsonRtePaths
-          );
-          referredUids.push(...blockRefUids);
-        }
-      } else if (
-        field.data_type === 'group' &&
-        field.schema &&
-        field.schema.length > 0
-      ) {
-        const { referredUids: groupRefUids } = this.extractUids(
-          field.schema,
-          [...fieldPath],
-          refPathMap,
-          jsonRtePaths
-        );
-        referredUids.push(...groupRefUids);
-      } else if (
-        field.data_type === 'json' &&
-        field.field_metadata?.allow_json_rte
-      ) {
-        const rtePath = [...pathPrefix, field.uid].join('.');
-        jsonRtePaths.push(rtePath);
-      }
-    }
-    return { referredUids, refPathMap };
-  }
+  // extractUids(schema, pathPrefix = [], refPathMap = {}, jsonRtePaths = []) {
+  //   const referredUids = [];
+  //   for (const field of schema) {
+  //     const fieldPath = [...pathPrefix, field.uid];
+  //     if (
+  //       field.data_type === 'reference' &&
+  //       Array.isArray(field.reference_to) &&
+  //       field.reference_to.length > 0
+  //     ) {
+  //       referredUids.push(...field.reference_to);
+  //       refPathMap[fieldPath.join('.')] = field.reference_to;
+  //     } else if (
+  //       field.data_type === 'blocks' &&
+  //       field.blocks &&
+  //       field.blocks.length > 0
+  //     ) {
+  //       for (const block of field.blocks) {
+  //         const { referredUids: blockRefUids } = this.extractUids(
+  //           block.schema,
+  //           [...fieldPath, block.uid],
+  //           refPathMap,
+  //           jsonRtePaths
+  //         );
+  //         referredUids.push(...blockRefUids);
+  //       }
+  //     } else if (
+  //       field.data_type === 'group' &&
+  //       field.schema &&
+  //       field.schema.length > 0
+  //     ) {
+  //       const { referredUids: groupRefUids } = this.extractUids(
+  //         field.schema,
+  //         [...fieldPath],
+  //         refPathMap,
+  //         jsonRtePaths
+  //       );
+  //       referredUids.push(...groupRefUids);
+  //     } else if (
+  //       field.data_type === 'json' &&
+  //       field.field_metadata?.allow_json_rte
+  //     ) {
+  //       const rtePath = [...pathPrefix, field.uid].join('.');
+  //       jsonRtePaths.push(rtePath);
+  //     }
+  //   }
+  //   return { referredUids, refPathMap };
+  // }
 
-  isNested(value) {
-    if (typeof value === 'object' && !Array.isArray(value) && value !== null) {
-      return true;
-    }
-    return false;
-  }
+  // isNested(value) {
+  //   if (typeof value === 'object' && !Array.isArray(value) && value !== null) {
+  //     return true;
+  //   }
+  //   return false;
+  // }
 
-  /**
-   * Identify reference paths in user-provided data
-   * @param {any} data - entry data
-   * @param {string[]} currentPath - traversal path
-   * @param {string[]} referenceFieldPaths - content type reference paths
-   */
-  identifyReferences(data, currentPath = [], referenceFieldPaths = []) {
-    const paths = [];
+  // /**
+  //  * Identify reference paths in user-provided data
+  //  * @param {any} data - entry data
+  //  * @param {string[]} currentPath - traversal path
+  //  * @param {string[]} referenceFieldPaths - content type reference paths
+  //  */
+  // identifyReferences(data, currentPath = [], referenceFieldPaths = []) {
+  //   const paths = [];
 
-    for (const [k, v] of Object.entries(data)) {
-      if (!v) {
-        continue;
-      }
-      if (currentPath.length > 0) {
-        const refPath = currentPath.join('.');
-        // if a reference path and not already collected, collect it
-        if (referenceFieldPaths.includes(refPath) && !paths.includes(refPath)) {
-          paths.push(refPath);
-        }
-      }
-      if (this.isNested(v)) {
-        const tempPath = [...currentPath];
-        tempPath.push(k);
-        const p = this.identifyReferences(v, tempPath, referenceFieldPaths);
-        paths.push(...p);
-      } else if (Array.isArray(v)) {
-        const tempPath = [...currentPath];
-        tempPath.push(k);
-        if (v.length > 0) {
-          // need to go over all refs since each of them could be of different
-          // content type and might contain refs
-          for (const val of v) {
-            const p = this.identifyReferences(
-              val,
-              tempPath,
-              referenceFieldPaths
-            );
-            paths.push(...p);
-          }
-        }
-        // no multiple ref present, Gatsby value -> [] (empty array)
-        // no single ref present, Gatsby value -> []
-        else if (
-          v.length === 0 &&
-          referenceFieldPaths.includes(tempPath.join('.'))
-        ) {
-          // it is a single or multiple ref
-          // also no idea what child references the user must be querying
-          // so need to get all child refs
-          paths.push(tempPath.join('.'));
-          const childRefPaths = referenceFieldPaths.filter(path =>
-            path.startsWith(tempPath)
-          );
-          paths.push(...childRefPaths);
-        }
-      }
-    }
-    return [...new Set(paths)];
-  }
+  //   for (const [k, v] of Object.entries(data)) {
+  //     if (!v) {
+  //       continue;
+  //     }
+  //     if (currentPath.length > 0) {
+  //       const refPath = currentPath.join('.');
+  //       // if a reference path and not already collected, collect it
+  //       if (referenceFieldPaths.includes(refPath) && !paths.includes(refPath)) {
+  //         paths.push(refPath);
+  //       }
+  //     }
+  //     if (this.isNested(v)) {
+  //       const tempPath = [...currentPath];
+  //       tempPath.push(k);
+  //       const p = this.identifyReferences(v, tempPath, referenceFieldPaths);
+  //       paths.push(...p);
+  //     } else if (Array.isArray(v)) {
+  //       const tempPath = [...currentPath];
+  //       tempPath.push(k);
+  //       if (v.length > 0) {
+  //         // need to go over all refs since each of them could be of different
+  //         // content type and might contain refs
+  //         for (const val of v) {
+  //           const p = this.identifyReferences(
+  //             val,
+  //             tempPath,
+  //             referenceFieldPaths
+  //           );
+  //           paths.push(...p);
+  //         }
+  //       }
+  //       // no multiple ref present, Gatsby value -> [] (empty array)
+  //       // no single ref present, Gatsby value -> []
+  //       else if (
+  //         v.length === 0 &&
+  //         referenceFieldPaths.includes(tempPath.join('.'))
+  //       ) {
+  //         // it is a single or multiple ref
+  //         // also no idea what child references the user must be querying
+  //         // so need to get all child refs
+  //         paths.push(tempPath.join('.'));
+  //         const childRefPaths = referenceFieldPaths.filter(path =>
+  //           path.startsWith(tempPath)
+  //         );
+  //         paths.push(...childRefPaths);
+  //       }
+  //     }
+  //   }
+  //   return [...new Set(paths)];
+  // }
 
   isCurrentEntryEdited(entryContentType) {
     return entryContentType === this.livePreviewConfig?.content_type_uid
   }
 
   async get(data) {
-    const receivedData = structuredClone(data);
+    const t0 = performance.now();
+    console.log("hello world")
+    const receivedData = data;
     const { live_preview } = this.stackSdk;
 
     let status = {
@@ -308,46 +310,54 @@ export class ContentstackGatsby {
       if (!receivedData.uid) {
         throw new Error("Entry data must contain uid for live preview")
       }
+      if(!receivedData.cslp__meta) {
+        throw new Error("Entry data must contain cslp__meta for live preview")
+      }
       const contentTypeUid = receivedData.__typename.split("_").slice(1).join("_")
       const entryUid = receivedData.uid;
 
       status = this.statusStorage.get(contentTypeUid) ?? { hasLivePreviewEntryFound: this.isCurrentEntryEdited(contentTypeUid) }
 
-      if (
-        isEmpty(this.referenceFields[contentTypeUid]) ||
-        isEmpty(this.jsonRteFields[contentTypeUid])
-      ) {
-        const { refPathMap, jsonRtePaths } = await this.extractReferences({
-          '': [contentTypeUid],
-        }, status);
-        // store reference paths
-        this.referenceFields[contentTypeUid] = refPathMap;
-        this.referenceFieldsStorage.set(
-          contentTypeUid,
-          this.referenceFields[contentTypeUid]
-        );
-        // store json rte paths
-        this.jsonRteFields[contentTypeUid] = jsonRtePaths;
-        this.jsonRteFieldsStorage.set(
-          contentTypeUid,
-          this.jsonRteFields[contentTypeUid]
-        );
-      }
+      // if (
+      //   isEmpty(this.referenceFields[contentTypeUid]) ||
+      //   isEmpty(this.jsonRteFields[contentTypeUid])
+      // ) {
+      //   const { refPathMap, jsonRtePaths } = await this.extractReferences({
+      //     '': [contentTypeUid],
+      //   }, status);
+      //   // store reference paths
+      //   this.referenceFields[contentTypeUid] = refPathMap;
+      //   this.referenceFieldsStorage.set(
+      //     contentTypeUid,
+      //     this.referenceFields[contentTypeUid]
+      //   );
+      //   // store json rte paths
+      //   this.jsonRteFields[contentTypeUid] = jsonRtePaths;
+      //   this.jsonRteFieldsStorage.set(
+      //     contentTypeUid,
+      //     this.jsonRteFields[contentTypeUid]
+      //   );
+      // }
 
-      let referencePaths = Object.keys(this.referenceFields[contentTypeUid]);
-      referencePaths = referencePaths.filter(field => !!field)
+      // let referencePaths = Object.keys(this.referenceFields[contentTypeUid]);
+      // referencePaths = referencePaths.filter(field => !!field)
 
-      const paths = this.identifyReferences(
-        receivedData,
-        [],
-        referencePaths
-      );
+      // const paths = this.identifyReferences(
+      //   receivedData,
+      //   [],
+      //   referencePaths
+      // );
 
       this.statusStorage.set(contentTypeUid, status)
 
       if (!status.hasLivePreviewEntryFound) {
+        console.log("not being edited")
         return receivedData
       }
+
+      const entryCslpMeta = receivedData.cslp__meta[entryUid];
+      const paths = entryCslpMeta && entryCslpMeta.referencePaths ? entryCslpMeta.referencePaths : [];
+      const jsonRtePaths = entryCslpMeta && entryCslpMeta.jsonRtePaths ? entryCslpMeta.jsonRtePaths : [];
 
       const entry = await this.stackSdk
         .ContentType(contentTypeUid)
@@ -359,12 +369,16 @@ export class ContentstackGatsby {
         if (this.config.jsonRteToHtml) {
           jsonToHTML({
             entry: entry,
-            paths: this.jsonRteFields[contentTypeUid] ?? [],
+            paths: jsonRtePaths,
           });
         }
+        const t1 = performance.now();
+        console.log(`get() time: ${t1 - t0} ms`)
         return entry;
       }
     }
+    const t1 = performance.now();
+    console.log(`get() time: ${t1 - t0} ms`)
     return receivedData;
   }
 }
